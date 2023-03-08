bfc743bab79002366e1ab831c2a0633aa9ea5639
Ruperth <ruperthnyagesoa@gmail.com>
2023-03-08 11:40:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    const myArray = [];
+    for (let i = 0; i<name.length; i++) {
+        const message =
+        `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+        myArray.push(message);
+    }
+    return myArray;
+}
+function countDown(num) {
+    do {
+        console.log(num);
+        num--;
+    }
+    while (num >= 0);
+}
